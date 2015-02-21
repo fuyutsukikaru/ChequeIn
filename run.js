d05d8e06ca39e429a7feca1980e82ae3beb136fa
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 11:46:22 -0800

    Trying to fix code errors

--- run.js
+++ run.js
@@ -10,11 +10,11 @@ app.get("/callback", function(req, res) {
   console.log("Handling the response");
   code = req.params.code;
   console.log(code);
-  request.post("https://api.venmo.com/v1/oauth/access_token?client_id=2386&code=" + code + "&client_secret=38vPZDCqWU5QcsGGz6VdCNgG6ntZGKug", function(req, res) {
+  /*request.post("https://api.venmo.com/v1/oauth/access_token?client_id=2386&code=" + code + "&client_secret=38vPZDCqWU5QcsGGz6VdCNgG6ntZGKug", function(req, res) {
     console.log(req);
     //console.log(res.user.first_name);
     //res.send(res.user.first_name);
-  });
+  });*/
 });
 
 app.listen(process.env.PORT || 3000);
