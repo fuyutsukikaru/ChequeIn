8c0c186c028f63769f7fe86ddbe08569a5e8ec68
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 06:58:09 -0800

    Attempt 3 to deploy html

--- run.js
+++ run.js
@@ -6,7 +6,7 @@ var request = require("request");
 
 var urlencodedParser = parser.urlencoded({ extended: false });
 
-app.use(express.static(__dirname + '/index.html'));
+app.use(express.static(__dirname));
 
 app.post('/login', urlencodedParser, function(req, res) {
   // Handle login via Venmo here
