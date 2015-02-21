0960053995c73c79693538eba6122b9634def50d
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 09:07:06 -0800

    Fixed a typo

--- run.js
+++ run.js
@@ -10,7 +10,7 @@ var urlencodedParser = parser.urlencoded({ extended: false });
 app.use(express.static(__dirname));
 
 function loginToVenmo() {
-  https.get('*', function (req, re) {
+  https.get('*', function (req, res) {
     res.redirect("https://api.venmo.com/v1/oauth/authorize?client_id=2386&scope=make_payments%20access_profile%20access_friends&response_type=code")
   });
 }
