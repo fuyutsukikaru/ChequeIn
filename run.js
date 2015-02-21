3506a5482f56e521e8e0a5498ce778844c83964b
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 11:51:05 -0800

    Fixing the get request

--- run.js
+++ run.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname));
 
 app.get("/callback", function(req, res) {
   console.log("Handling the response");
-  code = req.params.code;
+  code = req.params(code);
   console.log(code);
   /*request.post("https://api.venmo.com/v1/oauth/access_token?client_id=2386&code=" + code + "&client_secret=38vPZDCqWU5QcsGGz6VdCNgG6ntZGKug", function(req, res) {
     console.log(req);
