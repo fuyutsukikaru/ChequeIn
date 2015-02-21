639926ac49391aa27605f399f79fa83fcbd0c966
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 13:24:08 -0800

    Used ejs

--- run.js
+++ run.js
@@ -5,17 +5,23 @@ var parser = require('body-parser');
 var request = require('request');
 
 app.use(express.static(__dirname));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 var info = "";
 
 app.get("/callback", function(req, res) {
   console.log("Handling the response");
   var code = req.query.code;
-  request.post("https://api.venmo.com/v1/oauth/access_token?client_id=2386&code=" + code + "&client_secret=38vPZDCqWU5QcsGGz6VdCNgG6ntZGKug", function(req, res, body) {
+  request.post("https://api.venmo.com/v1/oauth/access_token?client_id=2386&code=" + code + "&client_secret=38vPZDCqWU5QcsGGz6VdCNgG6ntZGKug", function(request, response, body) {
     info = JSON.parse(body);
-    res.render("/search", {
-      username = info.user.display_name
-    }
+    res.send("GET request to index");
+  });
+});
+
+app.get('/', function(req, res) {
+  return res.render("/search", {
+    username = info.user.display_name
   });
 });
 
