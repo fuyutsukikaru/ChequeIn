b2e3be11e6f8314eddf368ca7f2351d596125854
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 07:16:29 -0800

    Changed debug statement

--- run.js
+++ run.js
@@ -12,8 +12,8 @@ app.post('/login', urlencodedParser, function(req, res) {
   // Handle login via Venmo here
   console.log("Handling login now");
   request("https://api.venmo.com/v1/oauth/authorize?client_id=2386&scope=make_payments%access_friends&response_type=code", function(err, resp, body) {
-    console.log(body);
-    //console.log(resp);
+    //console.log(body);
+    console.log(resp);
   });
 });
 
