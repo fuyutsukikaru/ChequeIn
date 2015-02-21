ad4d7bb55c0d6a0b04248b2025e4032f6e102a97
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 07:41:13 -0800

    Trying new oauth flow

--- run.js
+++ run.js
@@ -3,6 +3,14 @@ var app = express();
 var parser = require('body-parser');
 
 var request = require("request");
+var qs = require('querystring'),
+    oauth = {
+      callback: 'http://chequein.herokuapp.com/callback/',
+      client_key: CONSUMER_KEY,
+      client_secret: CONSUMER_SECRET
+    },
+    url = 'https://api.venmo.com/v1/oauth/access_token';
+var openurl = require('openurl');
 
 var urlencodedParser = parser.urlencoded({ extended: false });
 
@@ -11,10 +19,11 @@ app.use(express.static(__dirname));
 app.post('/login', urlencodedParser, function(req, res) {
   // Handle login via Venmo here
   console.log("Handling login now");
-  request.get("https://api.venmo.com/v1/oauth/authorize?client_id=2386&scope=make_payments%access_friends&response_type=code", function(err, resp, body) {
-    console.log(body);
-    console.log(resp);
-  });
+  //request.get("https://api.venmo.com/v1/oauth/authorize?client_id=2386&scope=make_payments%access_friends&response_type=code", {
+    //console.log(body);
+    //console.log(resp);
+  //});
+  openurl.open("https://api.venmo.com/v1/oauth/authorize?client_id=2386&scope=make_payments%access_friends&response_type=code");
 });
 
 app.listen(process.env.PORT || 3000);
