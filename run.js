aa15b5d5ee81fb9dbf8168cf00e2ebf85385b0b5
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 09:12:17 -0800

    Trying writing the html

--- run.js
+++ run.js
@@ -3,15 +3,14 @@ var app = express();
 var parser = require('body-parser');
 
 var request = require('request');
-var https = require('https');
 
 var urlencodedParser = parser.urlencoded({ extended: false });
 
 app.use(express.static(__dirname));
 
 function loginToVenmo() {
-  https.get('*', function (req, res) {
-    res.redirect("https://api.venmo.com/v1/oauth/authorize?client_id=2386&scope=make_payments%20access_profile%20access_friends&response_type=code")
+  request.get("https://api.venmo.com/v1/oauth/authorize?client_id=2386&scope=make_payments%20access_profile%20access_friends&response_type=code", function(err, resp, body) {
+    resp.write(body);
   });
 }
 
