ba7263c086288e56e57e142392a63de7fae23a1f
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 11:52:34 -0800

    Trying to parse JSON

--- run.js
+++ run.js
@@ -8,13 +8,13 @@ app.use(express.static(__dirname));
 
 app.get("/callback", function(req, res) {
   console.log("Handling the response");
-  code = req.params(code);
+  code = req.param(code);
   console.log(code);
-  /*request.post("https://api.venmo.com/v1/oauth/access_token?client_id=2386&code=" + code + "&client_secret=38vPZDCqWU5QcsGGz6VdCNgG6ntZGKug", function(req, res) {
+  request.post("https://api.venmo.com/v1/oauth/access_token?client_id=2386&code=" + code + "&client_secret=38vPZDCqWU5QcsGGz6VdCNgG6ntZGKug", function(req, res) {
     console.log(req);
-    //console.log(res.user.first_name);
-    //res.send(res.user.first_name);
-  });*/
+    console.log(res.user.first_name);
+    res.send(res.user.first_name);
+  });
 });
 
 app.listen(process.env.PORT || 3000);
