5abe9edb8d4a844e4c025c82f8b634b520d72d33
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 12:28:19 -0800

    Fixed getting the code

--- run.js
+++ run.js
@@ -12,10 +12,9 @@ app.get("/callback", function(req, res) {
   console.log("The code is " + code);
   console.log("https://api.venmo.com/v1/oauth/access_token?client_id=2386&code=" + code + "&client_secret=38vPZDCqWU5QcsGGz6VdCNgG6ntZGKug");
   request.post("https://api.venmo.com/v1/oauth/access_token?client_id=2386&code=" + code + "&client_secret=38vPZDCqWU5QcsGGz6VdCNgG6ntZGKug", function(req, res) {
-    //var info = JSON.parse(body);
-    //console.log(info);
-    //console.log(info.user.first_name);
-    //res.send(info.user.first_name);
+    var info = JSON.parse(body);
+    console.log(info);
+    console.log(info.user.first_name);
   });
 });
 
