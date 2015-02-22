e9d5b314f9b6f4825a183b3bb530c04e46722a85
Soomin Jeong <sjeongus@gmail.com>
2015-02-21 23:31:18 -0800

    Refactored code

--- run.js
+++ run.js
@@ -15,15 +15,16 @@ app.get("/callback", function(req, res) {
   var code = req.query.code;
   request.post("https://api.venmo.com/v1/oauth/access_token?client_id=2386&code=" + code + "&client_secret=38vPZDCqWU5QcsGGz6VdCNgG6ntZGKug", function(request, response, body) {
     info = JSON.parse(body);
-  });
-  app.get('/', function(req, res) {
-    res.send("GET request");
-    res.render("/search.ejs", {
-      username: info.user.display_name
-    });
+    response.send("Got GET request");
   });
 });
 
+app.get('/', function(req, res) {
+  res.send("GET request");
+  res.render("/search.ejs", {
+    username: info.user.display_name
+  });
+});
 
 app.listen(process.env.PORT || 3000);
 
