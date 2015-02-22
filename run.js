f4ba6fb367ddfff8fad6c294eeed7aed73ba3e65
Soomin Jeong <sjeongus@gmail.com>
2015-02-22 05:39:20 -0800

    Printing data

--- run.js
+++ run.js
@@ -31,16 +31,14 @@ app.get("/callback", function(req, res) {
 });
 
 app.post("/location", urlParser, function(req, res) {
-  yelp.search({term: "food", location: "Palo Alto", cll: req.body.latitude + "," + req.body.longitude}, function (err, data) {
-    //console.log(data);
+  yelp.search({term: "food", location: "Palo Alto", cll: req.body.latitude + "," + req.body.longitude, limit: 10, sort: 1, radius_filter: 5000}, function (err, data) {
+    console.log(data);
     business = data.businesses;
-    console.log(business);
     var total = data.total;
     var names = [];
     for (var i = 0; i < total; i++) {
       names.push(business.name);
     }
-    console.log(names);
     res.render("location", {
       username: info.user.display_name,
       place: names,
