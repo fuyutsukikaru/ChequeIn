6bdddfedf4b9d2724372e78e3eed9531ea3c9339
Soomin Jeong <sjeongus@gmail.com>
2015-02-22 10:04:12 -0800

    More changes

--- run.js
+++ run.js
@@ -33,7 +33,7 @@ app.post("/location", urlParser, function(req, res) {
   yelp.search({term: "food", location: "Palo Alto", cll: req.body.latitude + "," + req.body.longitude, limit: 10, sort: 1, radius_filter: 5000}, function (err, data) {
     //console.log(data);
     var business = data.businesses;
-    console.log(business);
+    //console.log(business);
     var total = data.total;
     var names = [];
     for (var i = 0; i < data.businesses.length; i++) {
