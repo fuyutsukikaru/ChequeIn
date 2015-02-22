80fd854d0856b8ffa6097393185b810d9d1b4690
Soomin Jeong <sjeongus@gmail.com>
2015-02-22 05:02:11 -0800

    Refined search

--- run.js
+++ run.js
@@ -30,7 +30,7 @@ app.get("/callback", function(req, res) {
 });
 
 app.post("/location", urlParser, function(req, res) {
-  yelp.search({location: "Palo Alto", cll: req.body.latitude + "," + req.body.longitude, limit: "10", sort: "1", radius_filter: "5000"}, function (err, data) {
+  yelp.search({term: "food", location: "Palo Alto", cll: req.body.latitude + "," + req.body.longitude, limit: 10, sort: 1, radius_filter: 5000}, function (err, data) {
     console.log(data);
   });
 });
